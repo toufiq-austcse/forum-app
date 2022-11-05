bd3ed9ff8e17fd2a4ab91edb13e4bd648a7dbf3b
toufiq <toufiq@10minuteschool.com>
2022-11-06 02:29:57 +0600

    Feature: Dockerize

--- be/post-service/src/common/database/database.module.ts
+++ be/post-service/src/common/database/database.module.ts
@@ -1,29 +1,28 @@
-import { Global, Module } from '@nestjs/common';
+import { Global, Logger, Module } from '@nestjs/common';
 import dataSource from 'ormconfig';
 import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
+import { AppConfigService } from '@common/app-config/service/app-config.service';
 
 @Global()
 @Module({
   providers: [
     {
       provide: DataSource,
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => {
-        await dataSource.initialize();
-        // const queryRunner = await db.createQueryRunner();
-        // try {
-        //   console.log('called');
-        //   if (dbName) {
-        //     let result = await queryRunner.manager.query(
-        //       `CREATE DATABASE IF NOT EXISTS ${dbName} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`
-        //     );
-        //     Logger.log(`${dbName} db created`);
-        //   }
-        //
-        // } catch (e) {
-        //   console.log('DB Cannot Created ', e);
-        // }
+      inject: [AppConfigService],
+      useFactory: async () => {
+        let db = await dataSource.initialize();
+        const queryRunner = await db.createQueryRunner();
+        try {
+          if (AppConfigService.appConfig.DB_NAME) {
+            await queryRunner.manager.query(
+              `CREATE DATABASE IF NOT EXISTS ${AppConfigService.appConfig.DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`
+            );
+            Logger.log(`${AppConfigService.appConfig.DB_NAME} db created`);
+          }
+
+        } catch (e) {
+          console.log('DB Cannot Created ', e);
+        }
 
         return dataSource;
       }
