da18c0b28602931c00ec20365299d3c43492cab1
toufiq <toufiq@10minuteschool.com>
2022-11-06 20:37:42 +0600

    Feature: Dockerize done

---
+++ README.md
@@ -0,0 +1,6 @@
+## Launch services
+
+- Execute `cp be/auth-service/env.example be/auth-service/.env && cp be/post-service/.env.example be/post-service/.env`
+- Execute `docker compose up -d`
+-  Hit http://localhost:3001/api-doc for Auth Service API Documentation
+- Hit http://localhost:3000/api-doc for Post Service API Documentation
--- be/post-service/src/common/database/database.module.ts
+++ be/post-service/src/common/database/database.module.ts
@@ -1,4 +1,4 @@
-import { Global, Logger, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import dataSource from 'ormconfig';
 import { DataSource } from 'typeorm';
 import { AppConfigService } from '@common/app-config/service/app-config.service';
@@ -10,20 +10,7 @@ import { AppConfigService } from '@common/app-config/service/app-config.service'
       provide: DataSource,
       inject: [AppConfigService],
       useFactory: async () => {
-        let db = await dataSource.initialize();
-        const queryRunner = await db.createQueryRunner();
-        try {
-          if (AppConfigService.appConfig.DB_NAME) {
-            await queryRunner.manager.query(
-              `CREATE DATABASE IF NOT EXISTS ${AppConfigService.appConfig.DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci`
-            );
-            Logger.log(`${AppConfigService.appConfig.DB_NAME} db created`);
-          }
-
-        } catch (e) {
-          console.log('DB Cannot Created ', e);
-        }
-
+        await dataSource.initialize();
         return dataSource;
       }
     }
